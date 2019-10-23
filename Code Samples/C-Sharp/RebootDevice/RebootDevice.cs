43fd6d14c41619592d9903f19df5659cf4819e20
Salmanhusen.sipai <salmanhusen.sipai@42gears.com>
2019-10-23 19:05:18 +0530

    Reboot device

--- Code Samples/C-Sharp/RebootDevice/RebootDevice.cs
+++ Code Samples/C-Sharp/RebootDevice/RebootDevice.cs
@@ -11,7 +11,7 @@ namespace RebootDevice
 {
     static class Constants
     {
-        public const string BaseURL = "https://suremdm.42gears.com";  // Your SureMDM domain
+        public const string BaseURL = "https://suremdm.42gears.com/api";  // Your SureMDM domain
         public const string Username = "Username"; // Your SureMDM username
         public const string Password = "Password"; // Your SureMDM password
         public const string ApiKey = "Your ApiKey"; // Your SureMDM apikey
