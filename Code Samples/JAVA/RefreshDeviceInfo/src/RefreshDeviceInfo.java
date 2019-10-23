b96f4d2c8137fce7450bd10306f4cbe62d725c1b
Salmanhusen.sipai <salmanhusen.sipai@42gears.com>
2019-10-23 16:41:56 +0530

    Refresh device info

---
+++ Code Samples/JAVA/RefreshDeviceInfo/src/RefreshDeviceInfo.java
@@ -0,0 +1,114 @@
+
+// Description: Applying dynamic job to refresh device info using SureMDM apis.
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import okhttp3.Credentials;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class RefreshDeviceInfo
+{
+
+    public class Constants
+    {
+	static final String baseURL = "https://suremdm.42gears.com";
+	static final String username = "username";
+	static final String password = "Password";
+	static final String apikey = "Your ApiKey";
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+	String deviceName = "device 1"; // Name of the device on which job will be applied
+	String DeviceID = GetDeviceID(deviceName);
+	if (DeviceID != null)
+	{
+	    String status = RefreshDevice(DeviceID);
+	    System.out.print(status);
+	}
+	else
+	{
+	    System.out.print("Device not found!");
+	}
+    }
+
+    private static String RefreshDevice(String deviceID) throws Exception
+    {
+	// ... API URL
+	String URL = Constants.baseURL + "/dynamicjob";
+	// ... PayLoad data
+	JSONObject PayLoad = new JSONObject();
+	PayLoad.put("DeviceID", deviceID);
+	PayLoad.put("JobType", "Refresh_Device");
+	// ... request body
+	MediaType mediaType = MediaType.parse("application/json");
+	RequestBody body = RequestBody.create(mediaType, PayLoad.toString());
+	// ... Create request
+	OkHttpClient client = new OkHttpClient();
+	Request request = new Request.Builder().url(URL)
+		// ... Send payload
+		.post(body)
+		// ... Basic authentication header
+		.addHeader("Authorization", Credentials.basic(Constants.username, Constants.password))
+		// ... ApiKey Header
+		.addHeader("ApiKey", Constants.apikey)
+		// ... Set content type
+		.addHeader("Content-Type", "application/json").build();
+	// ... Execute request
+	Response response = client.newCall(request).execute();
+
+	return response.body().string();
+    }
+
+    private static String GetDeviceID(String deviceName) throws Exception
+    {
+	// request body
+	JSONObject PayLoad = new JSONObject();
+	PayLoad.put("ID", "AllDevices");
+	PayLoad.put("IsSearch", true);
+	PayLoad.put("Limit", 20);
+	PayLoad.put("SearchColumns", new JSONArray("[\"DeviceName\"]"));
+	PayLoad.put("SearchValue", deviceName);
+	PayLoad.put("SortColumn", "LastTimeStamp");
+	PayLoad.put("SortOrder", "asc");
+	MediaType mediaType = MediaType.parse("application/json");
+	RequestBody body = RequestBody.create(mediaType, PayLoad.toString());
+
+	// API URL
+	String URL = Constants.baseURL + "/device";
+	// Create request
+	OkHttpClient client = new OkHttpClient();
+	Request request = new Request.Builder().url(URL)
+		// Send payload
+		.post(body)
+		// Basic authentication header
+		.addHeader("Authorization", Credentials.basic(Constants.username, Constants.password))
+		// ApiKey Header
+		.addHeader("ApiKey", Constants.apikey)
+		// Set content type
+		.addHeader("Content-Type", "application/json").build();
+	// Execute request
+	Response response = client.newCall(request).execute();
+	// Extracting DeviceID
+	String data = response.body().string();
+	if (response.isSuccessful())
+	{
+	    JSONObject jsonObj = new JSONObject(data);
+	    JSONArray devices = jsonObj.getJSONArray("rows");
+	    for (int index = 0; index < devices.length(); index++)
+	    {
+		JSONObject device = devices.getJSONObject(index);
+		if (device.get("DeviceName").equals(deviceName))
+		{
+		    return device.get("DeviceID").toString();
+		}
+	    }
+	}
+	return null;
+    }
+
+}
---
+++ Code Samples/Python/RefreshDeviceInfo.py
@@ -0,0 +1,71 @@
+import requests,json
+
+baseurl="https://suremdm.42gears.com/api"    # BaseURL of SureMDM
+Username="Username"
+Password="Password"
+ApiKey="Your ApiKey"
+
+# Method for getting device info
+def GetDeviceID(deviceName):
+    # Api url
+    url = baseurl+"/device"
+    # Headers
+    headers = {
+        # Api-Key header
+        'ApiKey': ApiKey,
+        # Set Content type
+        'Content-Type': "application/json",
+        }
+    # Basic authentication credentials
+    Credentials=(Username,Password)
+    # Request body
+    PayLoad={
+        "ID" : "AllDevices",
+        "IsSearch" : bool(1),
+        "Limit" : 10,
+        "SearchColumns" : ["DeviceName"],
+        "SearchValue" : deviceName,
+        "SortColumn" : "LastTimeStamp",
+        "SortOrder" : "asc"
+    }
+    # Executing request
+    response = requests.post(url,auth=Credentials,json=PayLoad,headers=headers)
+    # Extracting required GroupID
+    if response.status_code == 200:
+        if response.text != '[]':
+            data = response.json()
+            for device in data['rows']:
+                if device['DeviceName'] == deviceName:
+                    return device["DeviceID"]
+    else:
+        return None
+
+# Method to apply dynamic jobs
+def RefreshDeviceInfo(deviceID):
+    PayLoad={
+        "JobType": "Refresh_Device",
+        "DeviceID": deviceID
+    }
+    # Api url
+    url = baseurl+"/dynamicjob"
+    # Headers
+    headers = {
+        # Api-Key header
+        'ApiKey': ApiKey,
+        # Set Content type
+        'Content-Type': "application/json",
+        }
+    # Basic authentication credentials
+    Credentials=(Username,Password)
+    # Executing request
+    response = requests.post(url,auth=Credentials,json=PayLoad,headers=headers)
+    return response.text
+
+# Main starts
+deviceName="device 1"
+DeviceID=GetDeviceID(deviceName)
+if DeviceID!=None:
+    status=RefreshDeviceInfo(DeviceID)
+    print(status)
+else:
+    print('Device not found!')
